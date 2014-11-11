6de6dc6e7169c14825a3831c119db484da5bf284
Dahiana <daanbavi@hotmail.com>
2014-11-11 15:56:43 -0500

    Se creo controladores para proyecto y contactohasentidad

---
+++ Gesproy/Gesproy/Controllers/ContactoHasEntidadController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CapaDatos.Modelo;
+
+namespace Gesproy.Controllers
+{
+    public class ContactoHasEntidadController : Controller
+    {
+        private bd_gesproyEntities db = new bd_gesproyEntities();
+
+        // GET: /ContactoEntidad/
+        public ActionResult Index()
+        {
+            var contactoentidadejecutora_has_entidadejecutora = db.contactoEntidadEjecutora_has_entidadEjecutora.Include(c => c.contactoEntidadEjecutora).Include(c => c.entidadEjecutora);
+            return View(contactoentidadejecutora_has_entidadejecutora.ToList());
+        }
+
+        // GET: /ContactoEntidad/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contactoEntidadEjecutora_has_entidadEjecutora contactoentidadejecutora_has_entidadejecutora = db.contactoEntidadEjecutora_has_entidadEjecutora.Find(id);
+            if (contactoentidadejecutora_has_entidadejecutora == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactoentidadejecutora_has_entidadejecutora);
+        }
+
+        // GET: /ContactoEntidad/Create
+        public ActionResult Create()
+        {
+            ViewBag.contactoEntidadEjecutora_idContactoEntidad1 = new SelectList(db.contactoEntidadEjecutora, "idContactoEntidad", "tipoId");
+            ViewBag.entidadEjecutora_idEntidadEjecutora1 = new SelectList(db.entidadEjecutora, "idEntidadEjecutora", "nit");
+            return View();
+        }
+
+        // POST: /ContactoEntidad/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="entidadEjecutora_idEntidadEjecutora1,contactoEntidadEjecutora_idContactoEntidad1,id")] contactoEntidadEjecutora_has_entidadEjecutora contactoentidadejecutora_has_entidadejecutora)
+        {
+            if (ModelState.IsValid)
+            {
+                db.contactoEntidadEjecutora_has_entidadEjecutora.Add(contactoentidadejecutora_has_entidadejecutora);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.contactoEntidadEjecutora_idContactoEntidad1 = new SelectList(db.contactoEntidadEjecutora, "idContactoEntidad", "tipoId", contactoentidadejecutora_has_entidadejecutora.contactoEntidadEjecutora_idContactoEntidad1);
+            ViewBag.entidadEjecutora_idEntidadEjecutora1 = new SelectList(db.entidadEjecutora, "idEntidadEjecutora", "nit", contactoentidadejecutora_has_entidadejecutora.entidadEjecutora_idEntidadEjecutora1);
+            return View(contactoentidadejecutora_has_entidadejecutora);
+        }
+
+        // GET: /ContactoEntidad/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contactoEntidadEjecutora_has_entidadEjecutora contactoentidadejecutora_has_entidadejecutora = db.contactoEntidadEjecutora_has_entidadEjecutora.Find(id);
+            if (contactoentidadejecutora_has_entidadejecutora == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.contactoEntidadEjecutora_idContactoEntidad1 = new SelectList(db.contactoEntidadEjecutora, "idContactoEntidad", "tipoId", contactoentidadejecutora_has_entidadejecutora.contactoEntidadEjecutora_idContactoEntidad1);
+            ViewBag.entidadEjecutora_idEntidadEjecutora1 = new SelectList(db.entidadEjecutora, "idEntidadEjecutora", "nit", contactoentidadejecutora_has_entidadejecutora.entidadEjecutora_idEntidadEjecutora1);
+            return View(contactoentidadejecutora_has_entidadejecutora);
+        }
+
+        // POST: /ContactoEntidad/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="entidadEjecutora_idEntidadEjecutora1,contactoEntidadEjecutora_idContactoEntidad1,id")] contactoEntidadEjecutora_has_entidadEjecutora contactoentidadejecutora_has_entidadejecutora)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactoentidadejecutora_has_entidadejecutora).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.contactoEntidadEjecutora_idContactoEntidad1 = new SelectList(db.contactoEntidadEjecutora, "idContactoEntidad", "tipoId", contactoentidadejecutora_has_entidadejecutora.contactoEntidadEjecutora_idContactoEntidad1);
+            ViewBag.entidadEjecutora_idEntidadEjecutora1 = new SelectList(db.entidadEjecutora, "idEntidadEjecutora", "nit", contactoentidadejecutora_has_entidadejecutora.entidadEjecutora_idEntidadEjecutora1);
+            return View(contactoentidadejecutora_has_entidadejecutora);
+        }
+
+        // GET: /ContactoEntidad/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contactoEntidadEjecutora_has_entidadEjecutora contactoentidadejecutora_has_entidadejecutora = db.contactoEntidadEjecutora_has_entidadEjecutora.Find(id);
+            if (contactoentidadejecutora_has_entidadejecutora == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactoentidadejecutora_has_entidadejecutora);
+        }
+
+        // POST: /ContactoEntidad/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            contactoEntidadEjecutora_has_entidadEjecutora contactoentidadejecutora_has_entidadejecutora = db.contactoEntidadEjecutora_has_entidadEjecutora.Find(id);
+            db.contactoEntidadEjecutora_has_entidadEjecutora.Remove(contactoentidadejecutora_has_entidadejecutora);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Gesproy/Gesproy/Controllers/ProyectoController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CapaDatos.Modelo;
+
+namespace Gesproy.Controllers
+{
+    public class ProyectoController : Controller
+    {
+        private bd_gesproyEntities db = new bd_gesproyEntities();
+
+        // GET: /Proyecto/
+        public ActionResult Index()
+        {
+            var proyecto = db.proyecto.Include(p => p.entidadEjecutora).Include(p => p.lis_detalle).Include(p => p.lis_detalle1);
+            return View(proyecto.ToList());
+        }
+
+        // GET: /Proyecto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            proyecto proyecto = db.proyecto.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // GET: /Proyecto/Create
+        public ActionResult Create()
+        {
+            ViewBag.entidadEjecutora_idEntidadEjecutora = new SelectList(db.entidadEjecutora, "idEntidadEjecutora", "nit");
+            ViewBag.lis_detalle_id_sector = new SelectList(db.lis_detalle, "id", "codigo");
+            ViewBag.lis_detalle_id_localizacion = new SelectList(db.lis_detalle, "id", "codigo");
+            return View();
+        }
+
+        // POST: /Proyecto/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="bpin,nombre,objetivo_general,ano_final,ano_inicial,fecha_creacion,entidadEjecutora_idEntidadEjecutora,ava_fisico,ava_financiero,num_beneficiarios,total_proyecto,lis_detalle_id_sector,lis_detalle_id_localizacion")] proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.proyecto.Add(proyecto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.entidadEjecutora_idEntidadEjecutora = new SelectList(db.entidadEjecutora, "idEntidadEjecutora", "nit", proyecto.entidadEjecutora_idEntidadEjecutora);
+            ViewBag.lis_detalle_id_sector = new SelectList(db.lis_detalle, "id", "codigo", proyecto.lis_detalle_id_sector);
+            ViewBag.lis_detalle_id_localizacion = new SelectList(db.lis_detalle, "id", "codigo", proyecto.lis_detalle_id_localizacion);
+            return View(proyecto);
+        }
+
+        // GET: /Proyecto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            proyecto proyecto = db.proyecto.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.entidadEjecutora_idEntidadEjecutora = new SelectList(db.entidadEjecutora, "idEntidadEjecutora", "nit", proyecto.entidadEjecutora_idEntidadEjecutora);
+            ViewBag.lis_detalle_id_sector = new SelectList(db.lis_detalle, "id", "codigo", proyecto.lis_detalle_id_sector);
+            ViewBag.lis_detalle_id_localizacion = new SelectList(db.lis_detalle, "id", "codigo", proyecto.lis_detalle_id_localizacion);
+            return View(proyecto);
+        }
+
+        // POST: /Proyecto/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="bpin,nombre,objetivo_general,ano_final,ano_inicial,fecha_creacion,entidadEjecutora_idEntidadEjecutora,ava_fisico,ava_financiero,num_beneficiarios,total_proyecto,lis_detalle_id_sector,lis_detalle_id_localizacion")] proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proyecto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.entidadEjecutora_idEntidadEjecutora = new SelectList(db.entidadEjecutora, "idEntidadEjecutora", "nit", proyecto.entidadEjecutora_idEntidadEjecutora);
+            ViewBag.lis_detalle_id_sector = new SelectList(db.lis_detalle, "id", "codigo", proyecto.lis_detalle_id_sector);
+            ViewBag.lis_detalle_id_localizacion = new SelectList(db.lis_detalle, "id", "codigo", proyecto.lis_detalle_id_localizacion);
+            return View(proyecto);
+        }
+
+        // GET: /Proyecto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            proyecto proyecto = db.proyecto.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // POST: /Proyecto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            proyecto proyecto = db.proyecto.Find(id);
+            db.proyecto.Remove(proyecto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
